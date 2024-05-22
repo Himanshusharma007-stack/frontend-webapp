b4db549a5160250dc06ad406df462c560f2ce143
Himanshu Sharma <hishrma00@gmail.com>
2024-05-22 20:42:11 +0530

    add-homepage

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,16 @@
-export default function Card() {
-    return (
-        <div>
-            
+import Rating from "./Ratings";
+
+export default function Card({obj}) {
+  return (
+    <div className="w-60 h-60 rounded-lg">
+      <img src={obj.image} className="object-cover rounded-lg" />
+      <div>
+        <div className="flex justify-center">
+          <Rating count={obj.rating} />
         </div>
-    )
+        <h1 className="text-lg font-bold">{obj.name}</h1>
+        <p>{obj.cuisine}</p>
+      </div>
+    </div>
+  );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,36 @@
 import Card from "../components/Card";
+import React, { useState, useEffect } from "react";
+import { getRestaurants } from "../services/Restaurants";
 
 export default function Homepage() {
+  const [restaurantList, setRestaurantList] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await getRestaurants();
+        setRestaurantList(response.data);
+      } catch (error) {
+        console.error("Error --> ", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <>
       <div className="text-center">
-        <h1 className="text-2xl">Book your food</h1>
-        <p className="uppercase">Save your time and save your money</p>
+        <div className="mb-8">
+          <h1 className="text-2xl font-bold">Book your food</h1>
+          <p className="uppercase">Save your time and save your money</p>
+        </div>
 
-        <div className="text-center mt-3">
+        <div className="text-center mt-2">
           <div className="flex justify-center items-center space-x-2">
             <input
               className="flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
@@ -21,6 +44,18 @@ export default function Homepage() {
               Search
             </button>
           </div>
+        </div>
+
+        <div className="pt-8 flex justify-center">
+          {loading ? (
+            <p>Loading...</p>
+          ) : (
+            <div className="flex flex-wrap justify-center gap-4 md:gap-12">
+              {restaurantList.map((resto) => (
+                <Card key={resto.id} obj={resto} />
+              ))}
+            </div>
+          )}
         </div>
       </div>
     </>
