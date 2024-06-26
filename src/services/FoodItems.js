f3bf5cac6ad053e7c5100dd205ff9d39d6d592cf
Himanshu Sharma <hishrma00@gmail.com>
2024-06-26 11:14:00 +0530

    himanshu:add-manual-form-validation

---
+++ src/services/FoodItems.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+// const API_BASE_URL = "https://backend-webapp-inky.vercel.app";
+const API_BASE_URL = "http://localhost:3000";
+
+const apiClient = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// Function to create foodItem
+export const createFoodItem = async (obj) => {
+  try {
+    const response = await apiClient.post("/foodItem/create", obj);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching restaurants:", error);
+    throw error.response.data;
+  }
+};
+
+export default apiClient;
