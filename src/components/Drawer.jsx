7f10658ced625778d395c1b46e1c5cbaab424b9a
Himanshu Sharma <hishrma00@gmail.com>
2024-07-24 10:08:44 +0530

    wip

--- src/components/Drawer.jsx
+++ src/components/Drawer.jsx
@@ -12,12 +12,14 @@ import { Link } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 import { createUser } from '../services/User'
 import localStorageFunctions from "../utils/localStorageFunctions.js";
+import { useLocation } from "react-router-dom";
 
 export default function DrawerComp() {
   const [open, setOpen] = React.useState(false);
   const openDrawer = () => setOpen(true);
   const closeDrawer = () => setOpen(false);
   const { loginWithRedirect, user, isAuthenticated } = useAuth0();
+  const location = useLocation();
 
   useEffect(() => {
     if (isAuthenticated) {
@@ -71,7 +73,7 @@ export default function DrawerComp() {
           </IconButton>
         </div>
         <List>
-          {!user?.picture && <ListItem>
+          {!user?.picture && location.pathname !== "/restaurant/items-list" && <ListItem>
             <Link className="font-bold block" onClick={() => loginWithRedirect()}>
               Login/Signup User
             </Link>
