445dd2bdd2963ff58b8875712a862a4e5e4bb231
Himanshu Sharma <hishrma00@gmail.com>
2024-08-08 17:48:13 +0530

    wip

--- src/pages/RestaurantItems.jsx
+++ src/pages/RestaurantItems.jsx
@@ -5,6 +5,13 @@ import { isUserAuthenticated } from "../services/Auth";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import localStorageFunctions from "../utils/localStorageFunctions.js";
+import {
+  Tabs,
+  TabsHeader,
+  TabsBody,
+  Tab,
+  TabPanel,
+} from "@material-tailwind/react";
 
 export default function RestaurantItems() {
   const location = useLocation();
@@ -14,6 +21,22 @@ export default function RestaurantItems() {
   const [filteredList, setFilteredList] = useState([]);
   const navigate = useNavigate();
   const [token, setToken] = useState(null);
+
+  const tabData = [
+    {
+      label: "Menu",
+      value: "menu",
+      desc: `It really matters and then like it really doesn't matter.
+      What matters is the people who are sparked by it. And the people 
+      who are like offended by it, it doesn't matter.`,
+    },
+    {
+      label: "Orders",
+      value: "orders",
+      desc: `Because it's about motivating the doers. Because I'm here
+      to follow my dreams and inspire other people to follow their dreams, too.`,
+    },
+  ];
 
   useEffect(() => {
     const storedToken = localStorageFunctions.getDatafromLocalstorage("token");
@@ -81,7 +104,28 @@ export default function RestaurantItems() {
         <p className="text-lg font-light">{data?.address}</p>
       </div>
       <div className="md:w-[60rem]">
-        <Table data={menuItems} getMenuByRestoId={() => getMenuByRestoId()} isLoading={isLoading} />
+        <Tabs value="menu">
+          <TabsHeader>
+            {tabData.map(({ label, value }) => (
+              <Tab key={value} value={value}>
+                {label}
+              </Tab>
+            ))}
+          </TabsHeader>
+          <TabsBody>
+            <TabPanel value={"menu"}>
+              <Table
+                data={menuItems}
+                getMenuByRestoId={() => getMenuByRestoId()}
+                isLoading={isLoading}
+              />
+            </TabPanel>
+            <TabPanel value={"orders"}>
+              <div>Inside orders</div>
+            </TabPanel>
+
+          </TabsBody>
+        </Tabs>
       </div>
     </>
   );
